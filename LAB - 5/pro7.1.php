306d5f53a5307efb39d9adc1fafd60655aa648b5
RajModhwadiya <256257160+RajModhwadiya@users.noreply.github.com>
2026-07-01 10:59:32 +0530

    Delete LAB - 5 directory

--- LAB - 5/pro7.1.php
+++
@@ -1,5 +0,0 @@
-<?php
-//1.7 Write a PHP program to include a file using include and require function
-echo "<h2>hello everyone in php programming lab!</h2>";
-echo "<p>thids massage is from pro7.1.php file.</p>";
-?>
