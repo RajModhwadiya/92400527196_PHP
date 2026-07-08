c987d831a83195bbec1c6f2f5ccdd179594cc055
RajModhwadiya <256257160+RajModhwadiya@users.noreply.github.com>
2026-07-08 11:37:12 +0530

    Add files via upload

---
+++ LAB - 7/pro2.4.php
@@ -0,0 +1,30 @@
+<?php
+//2.4 Write a that demonstrate the use of following string functions:
+    $name="Raj Modhwadiya";
+
+    //1) strlen() // any string
+    echo strlen($name);
+    echo "<br></br>";
+
+    //2) strpos() //find some specific word,letter
+    echo strpos($name,"M",0);
+    echo "<br></br>";
+
+    //3) str_word_count()
+    echo str_word_count($name);
+    echo "<br></br>";
+
+    //4) strrev()
+    echo strrev($name);
+    echo "<br></br>";
+
+    //5) strtolower()
+    echo strtolower($name);
+    echo "<br></br>";
+
+    //6) strtoupper()
+    echo strtoupper($name);
+
+?>
+
+
