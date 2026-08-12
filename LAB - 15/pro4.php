e0babb45e45104d61f4e083f550097af01d563d4
RajModhwadiya <256257160+RajModhwadiya@users.noreply.github.com>
2026-08-12 23:16:44 +0530

    Add files via upload

---
+++ LAB - 15/pro4.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HTTP Headers</title>
+</head>
+<body>
+
+    <button onclick="getHeaders()">Get Headers</button>
+
+    <p id="result"></p>
+
+    <script>
+        function getHeaders() {
+
+            var xhr = new XMLHttpRequest();
+
+            xhr.open("GET", "text.txt", true);
+
+            xhr.onload = function () {
+
+                document.getElementById("result").innerHTML =
+                    "Content-Type: " + xhr.getResponseHeader("Content-Type") + "<br>" +
+                    "Content-Length: " + xhr.getResponseHeader("Content-Length") + "<br>" +
+                    "Last-Modified: " + xhr.getResponseHeader("Last-Modified") + "<br>" +
+                    "ETag: " + xhr.getResponseHeader("ETag");
+
+            };
+
+            xhr.send();
+
+        }
+    </script>
+
+</body>
+</html>
